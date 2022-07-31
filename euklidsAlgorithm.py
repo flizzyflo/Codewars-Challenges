56e741ffa9425dcfd802250385dafd248a707c94
flizzyflo <vip3r1@gmx.de>
2022-07-31 11:28:31 +0200

    upload euklids algorithm

---
+++ euklidsAlgorithm.py
@@ -0,0 +1,20 @@
+
+def euklidsAlgorithm(a: int, b: int) -> int:
+
+    """Algorithm takes two integers as input. Calculates the greatest common divisor 
+    of both integers"""
+
+    if b > a:
+        b, a = a, b
+
+    while True:
+        c = a % b
+
+        a = b
+        b = c
+        
+        if c <= 0:
+            return a
+
+
+print(euklidsAlgorithm(64, 34))
