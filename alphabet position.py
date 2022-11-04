30faa511301f9a2389b93222dc59f5f670fdd3b4
flizzyflo <102415350+flizzyflo@users.noreply.github.com>
2022-11-04 11:14:07 +0100

    Add files via upload

---
+++ alphabet position.py
@@ -0,0 +1,12 @@
+import string
+
+def alphabet_position(text: str) -> list[(str, int)]:
+
+    """Insert a string as text. Function will return a list of tuples containing the letter and the position within the alphabet."""
+
+    lower_case_letters= [letter for letter in string.ascii_lowercase]
+    letter_position = {letter: position + 1  for position, letter in enumerate(lower_case_letters)}
+
+    return list((letter, letter_position[letter]) for letter in text.lower() if letter in letter_position.keys())
+
+print(alphabet_position("florian"))
