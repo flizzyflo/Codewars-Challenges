049d1fab5d4a987737e4c2b8de65f06a1e52bd35
flizzyflo <vip3r1@gmx.de>
2023-01-10 23:42:51 +0100

    refactoring of several files

--- camel casing.py
+++ camel casing.py
@@ -2,7 +2,10 @@
 
 def solution(camel_case_string: str) -> str:
 
+    """Takes in a string as argument. Splits the string if a letter is upper case."""
+
     result_string: str = camel_case_string[0]
+
     for letter in camel_case_string[1:]:
         if letter.isupper():
             result_string += f' {letter}'
@@ -10,4 +13,7 @@ def solution(camel_case_string: str) -> str:
             result_string += letter
 
     return result_string
+
+
+print(solution("TeSt"))
 
