086a420b75f09851ae59160903e909b01f71a50c
flizzyflo <vip3r1@gmx.de>
2023-03-02 19:10:03 +0100

    initial upload

--- detect pangram.py
+++ detect pangram.py
@@ -1,15 +1,12 @@
 
 import string 
 
-def is_pangram(strng: str) -> bool:
+def is_pangram(input_string: str) -> bool:
 
     """Checks if a given string is a pangram. 
     A pangram is a stream of letters which includes all letters of a given alphabet."""
 
-    alphabet_lower: str = string.ascii_lowercase
+    lowercase_alphabet: str = set(string.ascii_lowercase)
+    string_set: str = {letter.lower() for letter in input_string if letter.strip() not in string.punctuation}
 
-    return {letter.lower() for letter in strng if letter.strip() not in string.punctuation} == set(alphabet_lower)
-
-
-
-print(is_pangram("The quick, brown fox jumps over the lazy dog!"))
+    return string_set == lowercase_alphabet
