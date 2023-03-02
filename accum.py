70bc9213055fce53b8d6d551b2a360856507fd51
flizzyflo <vip3r1@gmx.de>
2023-03-02 18:46:28 +0100

    initial upload

--- accum.py
+++ accum.py
@@ -1,8 +1,15 @@
 
 def accum(input_string: str) -> str:
+
+    """
+    Function splits string and prints the letter n-times within the new word, where
+    n is the index position of the letter wihtin the word, starting with
+    index number 1. Capitalizes the first letter of the word if its a letter.
+    @param input_string: String of letters and/ or numbers.
+    @return: New composed string. 'New' will become 'N-Ee-Www' for example.
+    """
+
     output_string = ""
     for i in range(len(input_string)):
         output_string += (input_string[i] * (i + 1) + "-")
     return output_string.title()[:-1]
-
-
--- add_two_integers.py
+++ add_two_integers.py
@@ -1,6 +1,13 @@
 
 def add_integer(array: list[int], target_number: int) -> tuple[int]:
+    """
+    Checks if the target number can be calculated from two numbers stored within the array.
+    If yes, returns a tuple containing the index position and the desired number.
 
+    @param array: Array containing integers to be summed up
+    @param target_number: Desired number which should be checked if its calculatable.
+    @return: Tuple of index position within the array and the number at indexposition.
+    """
     #stores number, index pair
     storage: dict[int, int] = { }
 
