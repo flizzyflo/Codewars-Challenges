0f09cf0fed771a3d1901f99c28ff92bb07fc1c0d
flizzyflo <vip3r1@gmx.de>
2023-04-11 19:43:06 +0200

    small refactoring

--- approximately_two.py
+++ approximately_two.py
@@ -1,13 +1,12 @@
 
 
-def approx_two(n: int, helper=0, list= [1, 1]) -> float:
+def approx_two(n: int, liste= [1, 1]) -> float:
     """Diese Funktion erzeugt eine Folge von Brüchen, die sich immer mehr der Wurzel von 2 annähern.
     Sie gibt das n-te Wertpaar aus, welches die Wurzel 2 approximiert."""
 
     for i in range(1, n):
-        helper = list[0]
-        list[0] = list[0] + list[1]
-        list[1] = list[0] + helper
-    return list[1]/list[0]
+        helper = liste[0]
+        liste[0] = liste[0] + liste[1]
+        liste[1] = liste[0] + helper
+    return liste[1]/liste[0]
 
-print(approx_two(15))
