7424b2453bdffe386c79aa466666a733d7371317
flizzyflo <vip3r1@gmx.de>
2023-04-11 19:43:00 +0200

    small refactoring

--- advanced_isogram.py
+++ advanced_isogram.py
@@ -1,33 +1,34 @@
 import string
 
+
 def is_isogram(word: str) -> bool:
     
     if not isinstance(word, str) or not word:
         return False
     
-    letter_occurence_dict = count_letters(word.lower())
+    letter_occurrence_dict = count_letters(word.lower())
     
-    return same_letter_occurence(letter_occurence_dict)
+    return same_letter_occurrence(letter_occurrence_dict)
 
 
 def count_letters(word: str) -> dict[str, int]:
     
-    letter_occurence_dict: dict[str, int] = dict()
+    letter_occurrence_dict: dict[str, int] = dict()
     
     for letter in word:
         
         if (letter == "") or (letter == " ") or (letter in string.punctuation):
             continue
         
-        elif letter in letter_occurence_dict.keys():
-            letter_occurence_dict[letter] += 1
+        elif letter in letter_occurrence_dict.keys():
+            letter_occurrence_dict[letter] += 1
 
         else:
-            letter_occurence_dict[letter] = 1
+            letter_occurrence_dict[letter] = 1
     
-    return letter_occurence_dict
+    return letter_occurrence_dict
             
             
-def same_letter_occurence(letter_occurence_dictionary: dict[str, int]) -> bool:
+def same_letter_occurrence(letter_occurrence_dictionary: dict[str, int]) -> bool:
     
-    return len(set(letter_occurence_dictionary.values())) == 1
+    return len(set(letter_occurrence_dictionary.values())) == 1
