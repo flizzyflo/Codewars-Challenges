0156302f5db4e93a1b3a63ba3824f4b8753e619d
flizzyflo <vip3r1@gmx.de>
2023-04-11 19:43:11 +0200

    small refactoring

--- bankersplan.py
+++ bankersplan.py
@@ -1,4 +1,9 @@
-def fortune(deposit: float, interest_rate: float, yearly_withdrawal: float, final_year: int, inflation_rate: float, current_year: int = 1):
+def fortune(deposit: float,
+            interest_rate: float,
+            yearly_withdrawal: float,
+            final_year: int,
+            inflation_rate: float,
+            current_year: int = 1):
     
     if current_year == final_year:
         return 0 <= deposit
@@ -7,11 +12,11 @@ def fortune(deposit: float, interest_rate: float, yearly_withdrawal: float, fina
     balance = deposit - yearly_withdrawal
     yearly_withdrawal = int(yearly_withdrawal * (1 + inflation_rate/100))
 
-    return fortune(deposit= balance, 
-                   interest_rate= interest_rate, 
-                   yearly_withdrawal= yearly_withdrawal, 
-                   final_year= final_year, 
-                   inflation_rate= inflation_rate, 
-                   current_year= current_year + 1)
+    return fortune(deposit=balance,
+                   interest_rate=interest_rate,
+                   yearly_withdrawal=yearly_withdrawal,
+                   final_year=final_year,
+                   inflation_rate=inflation_rate,
+                   current_year=current_year + 1)
 
    
